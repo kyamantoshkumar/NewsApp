5dec1e97d0afc2f268ec054cfab5e55bf13809c5
Mantosh Kumar Yadav <99826292+kyamantoshkumar@users.noreply.github.com>
2022-07-31 12:52:33 +0530

    Add files via upload

---
+++ src/FunctionalComponents/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='background text-light text-center p-5 mt-2'>
+            copyright@newsapp.com
+        </div>
+    )
+}
